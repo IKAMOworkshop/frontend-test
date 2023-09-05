edb81ead56471412935cf2eb14f719585a14198b
Jeremy Chang <changjeremy0226@gmail.com>
2023-09-05 05:37:49 -0700

    Smooth scrolling implemented

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,81 +1,98 @@
 <template>
     <canvas id="bg" ref="experience"></canvas>
-    <header class="landing-hero">
-        <div class="hero-row">
-            <h1 class="hero-title">></h1>
-            <h1 class="hero-title">HELLO THERE</h1>
+    <main id="element" data-scroll-container>
+        <div class="landing-hero" data-scroll-section>
+            <div class="hero-row">
+                <h1 class="hero-title">></h1>
+                <h1 class="hero-title">HELLO THERE</h1>
+            </div>
+            <div class="hero-row">
+                <h1 class="hero-title">I'M JEREMY</h1>
+                <h1 class="hero-title">!</h1>
+            </div>
+            <div class="hero-row">
+                <h1 class="hero-title">></h1>
+                <h1 class="hero-title">I DESIGN</h1>
+            </div>
+            <div class="hero-row">
+                <h1 class="hero-title">AND DEVELOP</h1>
+                <h1 class="hero-title">.</h1>
+            </div>
         </div>
-        <div class="hero-row">
-            <h1 class="hero-title">I'M JEREMY</h1>
-            <h1 class="hero-title">!</h1>
+        <div data-scroll-section>
+            <section class="work-title-section">
+                <h2 class="landing-section-title">WORKS.</h2>
+            </section>
+            <section class="landing-project-section">
+                <projectTile />
+            </section>
         </div>
-        <div class="hero-row">
-            <h1 class="hero-title">></h1>
-            <h1 class="hero-title">I DESIGN</h1>
-        </div>
-        <div class="hero-row">
-            <h1 class="hero-title">AND DEVELOP</h1>
-            <h1 class="hero-title">.</h1>
-        </div>
-    </header>
-    <main>
-        <section class="work-title-section">
-            <h2 class="landing-section-title">WORKS.</h2>
-        </section>
-        <section class="landing-project-section">
-            <projectTile />
-        </section>
     </main>
-  </template>
-  
-  <script setup>
-    import projectTile from '@/components/projectTile.vue'
+</template>
 
-    import * as THREE from 'three';
-    import { onMounted } from 'vue';
+<script>
+    import luxy from 'luxy.js';
 
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000,);
-    camera.position.set(0,5,20);
-    camera.rotation.set(0,0,0);
+    export default {
+        name: 'App',
+        mounted() {
+            luxy.init({
+                wrapper: '#element',
+                wrapperSpeed: 0.050,
+            });
+    }
+    };
+</script>
 
-    const box = new THREE.Mesh(
-        new THREE.BoxGeometry(8,8,8),
-        new THREE.MeshStandardMaterial({color: 0x008080}),
-    );
+<script setup>
+import projectTile from '@/components/projectTile.vue'
 
-    const directionLight = new THREE.DirectionalLight(0xffffff,1);
-    directionLight.rotation.set(0,45,-45);
+// Three JS
+import * as THREE from 'three';
+import { onMounted } from 'vue';
 
-    scene.add(camera, box, directionLight);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000,);
+camera.position.set(0,5,20);
+camera.rotation.set(0,0,0);
 
-    onMounted(() => {
-        const renderer = new THREE.WebGLRenderer({
-            canvas: document.querySelector('#bg'),
-            antialias: true,
-        });
+const box = new THREE.Mesh(
+    new THREE.BoxGeometry(8,8,8),
+    new THREE.MeshStandardMaterial({color: 0x008080}),
+);
 
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.setClearColor(0x181B1E,1)
+const directionLight = new THREE.DirectionalLight(0xffffff,1);
+directionLight.rotation.set(0,45,-45);
 
-        window.addEventListener('resize', function(){
-            const width = window.innerWidth;
-            const height = window.innerHeight;
-            renderer.setSize(width, height);
-            camera.aspect = width / height;
-            camera.updateProjectionMatrix();
-        });
+scene.add(camera, box, directionLight);
 
-        function animate(){
-            requestAnimationFrame(animate);
-            box.rotation.x += 0.01;
-            box.rotation.y += 0.005;
-            box.rotation.z += 0.01;
-
-            renderer.render(scene, camera);
-        };
-
-        animate();
+onMounted(() => {
+    const renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector('#bg'),
+        antialias: true,
     });
-  </script>
+
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x181B1E,1)
+
+    window.addEventListener('resize', function(){
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        renderer.setSize(width, height);
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+    });
+
+    function animate(){
+        requestAnimationFrame(animate);
+        box.rotation.x += 0.01;
+        box.rotation.y += 0.005;
+        box.rotation.z += 0.01;
+
+        renderer.render(scene, camera);
+    };
+
+    animate();
+});
+</script>
